657b1a88b29692f8366806d5d06f1e4755a4d685
devanshu-scaler <dev.jotwani14@gmail.com>
2024-06-15 23:29:14 +0530

    Task2 User Authentication and Authorization

--- ecommerce/models/user.py
+++ ecommerce/models/user.py
@@ -1,24 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
     email = models.EmailField(unique = True)
-    user = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, default= 'default_username',unique =True, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
     default_shipping_address = models.ForeignKey(
         "ShippingAddress",
         on_delete=models.DO_NOTHING,
         null=True,
+        blank=True,
         related_query_name="user_info"
     )
     default_billing_address = models.ForeignKey(
         "BillingAddress",
         on_delete=models.DO_NOTHING,
         null=True,
+        blank=True,
         related_query_name="user_info"
     )
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
 
 
 class Address(models.Model):
